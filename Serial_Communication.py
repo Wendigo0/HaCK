77f3c878f8afe502af4babf23b2be1ecbcad3c89
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-19 09:50:03 -0700

    Update Serial_Communication.py

--- Serial_Communication.py
+++ Serial_Communication.py
@@ -2,3 +2,41 @@ import serial
 import time
 import schedule
 
+
+list_values = []
+list_float_values = []
+
+
+def comm_w_arduino():
+    arduino = serial.Serial('com3', 9600)
+    print('Connection Established')
+    ard_data = arduino.readline()
+
+    decoded_value = str(ard_data[0:len(ard_data)].decode('utf-8'))
+    # Remove the random new line formatting
+    decoded_value = [x.rstrip() for x in decoded_value]
+    new = ''
+    # Put the string back together
+    for x in decoded_value:
+        new += x
+        
+    decoded_value = new
+    list_values.append(decoded_value)
+    print(list_values)
+    print(f'Collected readings from Arduino: {list_values}')
+
+    ard_data = 0
+    list_values.clear()
+    list_float_values.clear()
+    arduino.close()
+    print('Connection closed')
+    print('<-------------------------------------->')
+
+
+print('Program Started')
+
+schedule.every(1).seconds.do(comm_w_arduino)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
