04badf6e7d6832a433c2d6f67bd8317bb17db8c7
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-19 13:06:27 -0700

    Update Serial_Communication.py

--- Serial_Communication.py
+++ Serial_Communication.py
@@ -1,38 +1,15 @@
-import serial
 import time
 import schedule
+import tkinter as tk
+import Car
 
 import Value_Functions as vf
 
 
 def main_func():
-    arduino = serial.Serial('com3', 9600)
-    print('Connection Established')
-    ard_data = arduino.readline()
-
-    list_values.clear()
-    decoded_value = str(ard_data[0:len(ard_data)].decode('utf-8'))
-    # Remove the random new line formatting
-    decoded_value = [x.rstrip() for x in decoded_value]
-    new = ''
-    # Put the string back together
-    for x in decoded_value:
-        new += x
-
-    # Turn string into float
-    new = new.split(',')
-
-    for x in new:
-        list_values.append(float(x))
-    print(f'Collected readings from Arduino: {list_values}')
-
+    vf.comm_w_arduino(list_values)
     # Stores Readings in a Master List
     vf.create_master_list(list_values, MASTER_VALUES)
-
-    ard_data = 0
-    arduino.close()
-    print('Connection closed')
-    print('<-------------------------------------->')
 
 
 list_values = []
