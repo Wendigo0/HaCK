b36aeb2285a06acaf0c35fcddaf02313e683de25
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-20 09:33:12 -0700

    Update Car.py

--- Car.py
+++ Car.py
@@ -22,27 +22,31 @@ class Car:
     def move(self, canvas, master_list):
         maxd = 750
         counter = 0
+        new_list = master_list.copy()
         while counter < 3:  # Scale the meter data to pixel data
-            master_list[counter] = ((master_list[counter]*100)/2)*10
+            new_list[counter] = ((new_list[counter]*100)/2)*10
             counter += 1
 
-        dist_inward = master_list[0] + 12.5/2
-        dist_forward = master_list[1] + 12.5/2
-        dist_outward = master_list[2] + 12.5/2
+        dist_inward = new_list[0] + 12.5/2
+        dist_forward = new_list[1] + 12.5/2
+        dist_outward = new_list[2] + 12.5/2
+        if dist_forward <= 750:
+            if new_list[3] == 1:  # North wall
+                self.x = maxd - dist_forward
+                self.y = dist_outward
+                self.draw(canvas)
+            elif new_list[3] == 2:  # East wall
+                self.x = maxd - dist_outward
+                self.y = maxd - dist_forward
+                self.draw(canvas)
+            elif new_list[3] == 3:  # South wall
+                self.x = dist_forward
+                self.y = maxd-dist_outward
+                self.draw(canvas)
+            elif new_list[4] == 4:  # West wall
+                self.x = dist_outward
+                self.y = dist_forward
+                self.draw(canvas)
+        else:
+            self.draw(canvas)
 
-        if master_list[3] == 1:  # North wall
-            self.x = maxd - dist_forward
-            self.y = dist_outward
-            self.draw(canvas)
-        elif master_list[3] == 2:  # East wall
-            self.x = maxd - dist_outward
-            self.y = maxd - dist_forward
-            self.draw(canvas)
-        elif master_list[3] == 3:  # South wall
-            self.x = dist_forward
-            self.y = maxd-dist_outward
-            self.draw(canvas)
-        elif master_list[4] == 4:  # West wall
-            self.x = dist_outward
-            self.y = dist_forward
-            self.draw(canvas)
