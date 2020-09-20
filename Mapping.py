152fbd5fb3868d09388d34684d8cc70e67feefc3
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-19 23:51:55 -0700

    Update Mapping.py

--- Mapping.py
+++ Mapping.py
@@ -18,30 +18,33 @@ class Point:
     def determine_pos(self, canvas, list_values):
         maxd = 750
         counter = 0
+        #
         while counter < 3:
             list_values[counter] = ((list_values[counter] * 100) / 2) * 10
             counter += 1
+        list_values[3] = int(list_values[3])
         # Inner sensor data
         dist_inward = list_values[0] + (12.5 / 2) * 10  # Correction factor for width of the car
         # Front sensor data
         dist_forward = list_values[1] + (12.5 / 2) * 10  # Correction factor for the length of the car
         # Outer sensor data
         dist_outward = list_values[2] + (12.5 / 2) * 10
-        if (maxd - 2 <= dist_inward <= maxd + 2) and list_values[3] == 1:  # and going east:
+
+        if list_values[3] == 1:  # and going east:
             self.x = maxd - dist_forward
-            self.y = maxd - (dist_inward+dist_outward)
+            self.y = dist_inward + dist_outward
             self.draw(canvas)
-        elif (maxd - 2 <= dist_inward <= maxd + 2) and list_values[3] == 2:  # and going south:
+        elif list_values[3] == 2:  # and going south:
             self.x = maxd - (dist_inward+dist_outward)
             self.y = maxd - dist_forward
             self.draw(canvas)
-        elif (maxd - 2 <= dist_inward <= maxd + 2) and list_values[3] == 3:  # and going west
-            self.x = maxd - dist_forward
+        elif list_values[3] == 3:  # and going west
+            self.x = dist_forward
             self.y = maxd - (dist_inward+dist_outward)
             self.draw(canvas)
-        elif (maxd - 2 <= dist_inward <= maxd + 2) and list_values[3] == 4:  # and going north
-            self.x = maxd - (dist_inward+dist_outward)
-            self.y = maxd - dist_forward
+        elif list_values[3] == 4:  # and going north
+            self.x = dist_inward + dist_outward
+            self.y = dist_forward
             self.draw(canvas)
         elif dist_inward > maxd or dist_inward < 0:
             pass
