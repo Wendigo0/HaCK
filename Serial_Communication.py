843f450dce71f5073b16f08279a415db42a75ed0
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-20 09:40:19 -0700

    Update Serial_Communication.py

--- Serial_Communication.py
+++ Serial_Communication.py
@@ -8,8 +8,8 @@ window = tk.Tk()
 canvas = tk.Canvas(window, width=750, height=750, bg='black')  # Make 1pixel = 2cm
 canvas.pack()
 car = Car(canvas)
+dots = Point(canvas)
 
-counter = 0
 list_values = []
 MASTER_VALUES = []
 
@@ -18,21 +18,22 @@ def main():
 
     # Gathers reading from arduino distance sensors
     vf.comm_w_arduino(list_values)
-    # Stores Readings in a Master List
+
+    # Prints values for reference
     print(list_values)
 
-    # Car moves based on the IMU reading
+    # Car moves based on the distance sensor readings
     car.move(canvas, list_values)
 
+    # Pauses the code with the map still showing
     window.bind("<space>", lambda event: vf.pause(event))
 
-    dots = Point(canvas)
     dots.determine_pos(canvas, list_values)
 
     window.after(10, main)
 
-
 # print('Program Started')
+
 
 main()
 window.mainloop()
