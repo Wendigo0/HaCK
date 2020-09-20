117d8ddfd317466ff2a08ed53c52ea33214ce6ae
Wendigo0 <71185099+Wendigo0@users.noreply.github.com>
2020-09-19 20:33:27 -0700

    Update FourDistanceReadings.ino

--- FourDistanceReadings/FourDistanceReadings.ino
+++ FourDistanceReadings/FourDistanceReadings.ino
@@ -10,9 +10,7 @@ const int trigPin3 = 22;
 const int echoPin3 = 23;
 long duration3;
 
-const int trigPin4 = 26;
-const int echoPin4 = 27;
-long duration4;
+const int wall = 1;
 
 void setup() {
   pinMode(trigPin1, OUTPUT);
@@ -21,8 +19,6 @@ void setup() {
   pinMode(echoPin2, INPUT);
   pinMode(trigPin3, OUTPUT);
   pinMode(echoPin3, INPUT);
-  pinMode(trigPin4, OUTPUT);
-  pinMode(echoPin4, INPUT);
   Serial.begin(9600);
   
 }
@@ -71,18 +67,7 @@ void loop() {
   Serial.print(distance3); // in meters
   Serial.print(',');
 
-  //Fourth Sensor
-  digitalWrite(trigPin4, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(trigPin4, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin4, LOW);
-
-  duration4 = pulseIn(echoPin4, HIGH);
-  double distance4 = (double)duration4 * 345 / 2 / 1000000;
-
-  Serial.println(distance4); // in meters
+  Serial.println(wall); // in meters
   delay(100);
   
 }
